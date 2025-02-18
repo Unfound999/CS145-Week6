a9c229477898c85d8ce8207be2d07724a0ad2215
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-02-18 08:25:01 -0800

    Created simple UI for each move

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -29,20 +29,50 @@ public class TowersOfHanoi {
         towers.put("towerB", 0);
         towers.put("towerC", 0);
 
-        hanoiSolve(diskAmount, source, dest, aux, towers);
+        hanoiSolve(diskAmount, source, dest, aux, towers, diskAmount);
     } //  end of main method
 
-    public static void hanoiSolve(int diskAmount, String source, String dest, String aux, HashMap<String, Integer> towers) {
+    public static void hanoiSolve(int diskAmount, String source, String dest, String aux, HashMap<String, Integer> towers, int maxDisk) {
 
         if (diskAmount == 0) {
             return;
         } //  end of if loop
 
-        hanoiSolve(diskAmount-1, source, aux, dest, towers);
+        hanoiSolve(diskAmount-1, source, aux, dest, towers, maxDisk);
         towers.put(source, towers.get(source) - 1);
         towers.put(dest, towers.get(dest) + 1);
+        hanoiUI(towers, maxDisk);
         System.out.printf("%s%d%s%s%s%s%n", "Moved disk ", diskAmount, " from ", source, " to ", dest);
-        hanoiSolve(diskAmount-1, aux, dest, source, towers);
+        hanoiSolve(diskAmount-1, aux, dest, source, towers, maxDisk);
 
     } // end of hanoiSolve method
+
+    public static void hanoiUI (HashMap<String, Integer> towers, int maxDisk) {
+        
+        int towerAHeight = towers.get("towerA");
+        int towerBHeight = towers.get("towerB");
+        int towerCHeight = towers.get("towerC");
+        
+        for (int i = 0; i < maxDisk; i++) {
+            if (towerAHeight < maxDisk) {
+                System.out.printf("%d%s", 0, " ");
+                towerAHeight++;
+            } else {
+                System.out.printf("%d%s", 1, " ");
+            }
+            if (towerBHeight < maxDisk) {
+                System.out.printf("%d%s", 0, " ");
+                towerBHeight++;
+            } else {
+                System.out.printf("%d%s", 1, " ");
+            }
+            if (towerCHeight < maxDisk) {
+                System.out.println(0);
+                towerCHeight++;
+            } else {
+                System.out.println( 1);
+            }
+        }
+    }
+
 } //  end of TowersOfHanoi class
