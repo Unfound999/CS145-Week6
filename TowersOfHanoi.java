fb51b7701e6d91013e47831bdd401c526311aee8
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-02-18 12:20:49 -0800

    Created new method that prints simple UI with recursion rather than with iteration

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -1,3 +1,9 @@
+//  TO DO: remove original iterative version of hanoiUI method & change name of "hanoiUITest" method
+//         Clean up code formatting a bit
+//         Add comments
+//         Add header comments at top of program
+
+
 import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -41,7 +47,11 @@ public class TowersOfHanoi {
         hanoiSolve(diskAmount-1, source, aux, dest, towers, maxDisk);
         towers.put(source, towers.get(source) - 1);
         towers.put(dest, towers.get(dest) + 1);
-        hanoiUI(towers, maxDisk);
+        int towerAHeight = towers.get("towerA");
+        int towerBHeight = towers.get("towerB");
+        int towerCHeight = towers.get("towerC");
+        System.out.println();
+        hanoiUITest(towers, maxDisk, maxDisk, towerAHeight, towerBHeight, towerCHeight);
         System.out.printf("%s%d%s%s%s%s%n", "Moved disk ", diskAmount, " from ", source, " to ", dest);
         hanoiSolve(diskAmount-1, aux, dest, source, towers, maxDisk);
 
@@ -76,5 +86,34 @@ public class TowersOfHanoi {
             }
         }
     }
+
+      public static void hanoiUITest(HashMap<String, Integer> towers, int maxDisk, int heightMax, int towerAH, int towerBH, int towerCH) {
+
+        if (maxDisk == 0) {
+            return;
+        }
+        
+        if (towerAH < heightMax) {
+            System.out.printf("%d%s", 0, " ");
+            towerAH++;
+        } else {
+            System.out.printf("%d%s", 1, " ");
+        }
+        if (towerBH < heightMax) {
+            System.out.printf("%d%s", 0, " ");
+            towerBH++;
+        } else {
+            System.out.printf("%d%s", 1, " ");
+        }
+        if (towerCH < heightMax) {
+            System.out.println(0);
+            towerCH++;
+        } else {
+            System.out.println( 1);
+        }
+
+        hanoiUITest(towers, maxDisk-1, heightMax, towerAH, towerBH, towerCH);
+
+    } 
 
 } //  end of TowersOfHanoi class
